f4c13671a26fbb09eb405b670b543f88c703d00d
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-29 20:01:24 +0530

    Code added

---
+++ frontend/src/components/Check.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Check = () => {
+  return (
+    
+    <div>
+      Lorem ipsum dolor sit, amet consectetur adipisicing elit. Itaque molestiae voluptatem corrupti voluptates quo quidem assumenda tempora beatae aliquid illum nam magnam eius exercitationem aut quia placeat esse consectetur laborum dignissimos voluptate, sed perspiciatis inventore cumque. Modi architecto magni, est excepturi consequuntur, quaerat cupiditate nostrum deserunt dolore molestias aspernatur illo nemo voluptate consequatur enim rerum optio laborum ratione nam recusandae assumenda quidem tenetur doloribus. Autem nisi debitis consequatur distinctio voluptatum aliquid quidem repellat et, expedita iure facilis mollitia cum nostrum placeat quas ad culpa illo in sit aspernatur earum laboriosam! Dignissimos veniam at amet consectetur quaerat asperiores eum? Amet, aspernatur.
+    </div>
+  )
+}
+
+export default Check

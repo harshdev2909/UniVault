528a4d5433d6378d271c9dcd7e28bcf098a45b30
harshdev2909 <harsh.dev2909@gmail.com>
2024-10-29 21:29:01 +0530

    Code added

--- frontend/src/components/ui/hero-parallax.jsx
+++ frontend/src/components/ui/hero-parallax.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { motion, useScroll, useTransform, useSpring } from "framer-motion";
 import { Link } from "react-router-dom";
 import { TypewriterEffectSmooth } from "../ui/ypewriter-effect"
+import { useAuthStore } from "../../store/authStore";
 export const HeroParallax = ({
   products
 }) => {
@@ -57,17 +58,18 @@ export const HeroParallax = ({
 };
 const words = [
     
-    {
+  {
       text: "UniVault.",
       className: "text-blue-500 dark:text-blue-500",
     },
   ];
-export const Header = () => {
+  export const Header = () => {
+  const { user, logout } = useAuthStore();
   return (
     (<div className="flex flex-col items-center justify-center h-[40rem]  ">
-    <p className="text-neutral-600 dark:text-neutral-200 text-xs sm:text-base  ">
-      The road to freedom starts from here
-    </p>
+    <h1 className="text-neutral-600 dark:text-neutral-200 text-xl sm:text-base  ">
+      Welcome {user.name}
+    </h1>
     <TypewriterEffectSmooth words={words} />
     <div className="flex flex-col md:flex-row space-y-4 md:space-y-0 space-x-0 md:space-x-4">
       <button className="w-40 h-10 rounded-xl bg-black border dark:border-white border-transparent text-white text-sm">
